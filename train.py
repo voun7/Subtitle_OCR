e7d332527a58c455b160e41ae6c80310f18a98a1
victor n <nwaeze_victor@yahoo.com>
2023-07-12 18:22:15 -0400

    minor update

--- train.py
+++ train.py
@@ -1,1 +1,8 @@
 # A file to leverage all other files and train a target PyTorch model.
+import data_setup
+import engine
+import model_builder
+import synthetic_data_gen
+import utils
+
+print(data_setup, engine, model_builder, utils, synthetic_data_gen)
