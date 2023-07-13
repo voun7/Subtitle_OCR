3ea0c2f5a9c764fd7494c28af4f0beb5454721ff
victor n <nwaeze_victor@yahoo.com>
2023-07-12 22:31:59 -0400

    dataset class modified

--- data_setup.py
+++ data_setup.py
@@ -2,46 +2,34 @@
 import os
 from pathlib import Path
 
-from torch.utils.data import DataLoader, random_split
-from torchvision.datasets.folder import default_loader
-from torchvision.datasets.vision import VisionDataset
-from torchvision.transforms import ToTensor
+import torch
+from torch.utils.data import Dataset, DataLoader, random_split
+from torchvision.io import read_image
 
 
-class TRDGDataset(VisionDataset):
-    def __init__(self, root: str, transform=None, target_transform=None) -> None:
-        super().__init__(root, transform, target_transform)
-        self.data_dir = Path(root)
-        self.transform = transform
-        self.target_transform = target_transform
-        self.loader = default_loader  # Responsible for loading an image from the given path and returning it as a PIL.
-        self.image_files, self.labels, self.bboxes = self.load_data()
+class TRDGDataset(Dataset):
+    def __init__(self, image_dir: str) -> None:
+        self.image_dir = Path(image_dir)
+        self.image_files, self.image_texts, self.bboxes = self.load_data()
 
     def __getitem__(self, index: int) -> tuple:
-        path, target, bboxes = self.image_files[index], self.labels[index], self.bboxes[index]
-        image = self.loader(str(path))
-        if self.transform:
-            image = self.transform(image)
-        return image, target, bboxes
+        image_path, image_text, image_bboxes = self.image_files[index], self.image_texts[index], self.bboxes[index]
+        image = read_image(str(image_path))
+        return image, image_text, image_bboxes
 
     def __len__(self) -> int:
         return len(self.image_files)
 
     def load_data(self) -> tuple:
         image_files, labels, bboxes = [], [], []
-        label_file = self.data_dir / "labels.txt"
-        label_lines = label_file.read_text(encoding="utf-8").splitlines()
-
-        for line in label_lines:
-            line = line.split()
-            image_file = self.data_dir / line[0]
-            if image_file.exists():
-                image_files.append(image_file)
-                labels.append(line[1])
-                bbox_file = str(image_file).replace('.jpg', '_boxes.txt')
-                bbox_data = self.load_bounding_boxes(bbox_file)
-                bboxes.append(bbox_data)
-
+        image_ext = ".jpg"
+        for image_file in self.image_dir.glob(f"*{image_ext}"):
+            image_files.append(image_file)
+            image_txt = image_file.stem.split('_')[0]
+            labels.append(image_txt)
+            bbox_file = str(image_file).replace(image_ext, '_boxes.txt')
+            bbox_data = self.load_bounding_boxes(bbox_file)
+            bboxes.append(bbox_data)
         return image_files, labels, bboxes
 
     @staticmethod
@@ -56,10 +44,21 @@ class TRDGDataset(VisionDataset):
         return bboxes
 
 
-def create_dataloaders(data_dir: str, batch_size: int, split_ratio: float = 0.8, shuffle: bool = True) \
+def collate_fn(batch):
+    """
+    The collate_fn parameter in the DataLoader allows you to specify a function
+    that controls the behavior of batching samples from your dataset.
+    """
+    image, labels, bboxes = zip(*batch)
+    # Stack images, labels, and bboxes into batches
+    image = torch.stack(image)
+    return image, labels, bboxes
+
+
+def create_dataloaders(image_dir: str, batch_size: int, split_ratio: float = 0.8, shuffle: bool = True) \
         -> tuple[DataLoader, DataLoader]:
     # Create an instance of the TRDGDataset.
-    dataset = TRDGDataset(data_dir, transform=ToTensor())
+    dataset = TRDGDataset(image_dir)
     # Perform train-test split.
     dataset_size = len(dataset)
     train_size = int(split_ratio * dataset_size)
@@ -68,6 +67,10 @@ def create_dataloaders(data_dir: str, batch_size: int, split_ratio: float = 0.8,
 
     num_workers = os.cpu_count()
     # Create the data loaders for training and testing.
-    train_dataloader = DataLoader(train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=shuffle)
-    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False)
+    train_dataloader = DataLoader(
+        train_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=shuffle, collate_fn=collate_fn
+    )
+    test_dataloader = DataLoader(
+        test_dataset, batch_size=batch_size, num_workers=num_workers, shuffle=False, collate_fn=collate_fn
+    )
     return train_dataloader, test_dataloader
