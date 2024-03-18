c9ce1ab617a6ef7925604cb6caf3c3cadc715a14
Victor N <nwaeze_victor@yahoo.com>
2024-03-18 08:38:38 -0400

    errors with synthetic images fixed

--- data/synthetic_data_gen.py
+++ data/synthetic_data_gen.py
@@ -5,26 +5,22 @@ def generate_trdg_images() -> None:
     """
     Use trdg package to generate images with text for training model.
     """
+    dataset_dir = r"C:\Users\Victor\Documents\Python Datasets\Subtitle_OCR"
+    lang = "en"
     command = [
         "trdg",
-        "--output_dir", "training_data/chinese_data/trdg_synthetic_images",  # The output directory.
-        "--language", "cn",  # The language to use.
-        "--count", "50",  # The number of images to be created.
+        "--output_dir", f"{dataset_dir}/{lang}/rec/synthetic images",  # The output directory.
+        "--language", lang,  # The language to use.
+        "--count", "1000",  # The number of images to be created.
         "--length", "18",  # Define how many words should be included in each generated sample.
         "--random",  # Define if the produced string will have variable word count (with --length being the maximum).
-        "--format", "100",  # Define the height of the produced images if horizontal, else the width.
-        "--width", "1900",  # Define the width of the resulting image
-        "--thread_count", "6",  # Define the number of thread to use for image generation.
+        "--thread_count", "15",  # Define the number of thread to use for image generation.
         "--background", "3",  # Background to use. 0: Gaussian Noise, 1: Plain white, 2: Quasi crystal, 3: Image.
-        "--image_dir", "training_data/#Background_images",  # Image directory to use when background is set to image.
-        "--output_bboxes", "1",  # Define if the generator will return bounding boxes for the text.
+        "--image_dir", f"{dataset_dir}/#Background_images",  # Image directory to use when background is set to image.
         "--text_color", "#FFFFFF",  # Text's color. "#000000,#FFFFFF" for black to white range.
-        "--space_width", "0",  # Define the width of the spaces between words
-        "--margins", "15,15,15,15",  # Define the margins around the text when rendered. In pixels.
-        "--fit",  # Apply a tight crop around the rendered text
+        # "--space_width", "0",  # Define the width of the spaces between words
+        "--name_format", "2",  # Define how the produced files will be named.
         # Rarely needed options.
-        # "--character_spacing", "0",  # Define the width of the spaces between characters. 2 means two pixels.
-        # "--random_sequences",  # Use random sequences as the source text for the generation.
         # "--font_dir", "data/#Fonts",  # Define a font directory to be used.
         # "--word_split",  # Split on words instead of on characters.
     ]
