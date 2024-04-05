e0c09d96214b1a6ae4d6d73ba6102dfefa858cff
Victor N <nwaeze_victor@yahoo.com>
2024-04-05 16:47:47 -0400

    det model updated

--- models/detection.py
+++ models/detection.py
@@ -5,7 +5,6 @@ Preferred Detection Models
 1. DB algorithm (Differentiable Binarization)
 2. DB++ algorithm
 """
-import numpy as np
 import torch.nn as nn
 
 
@@ -25,27 +24,3 @@ class DBPP(nn.Module):
 
     def forward(self, x):
         pass
-
-
-def find_conv2d_out_shape(h_in, w_in, conv, pool=2):
-    """
-    :param h_in: an integer representing the height of input data
-    :param w_in: an integer representing the width of input data
-    :param conv:  an object of the CNN layer
-    :param pool: an integer representing the pooling size and default to 2
-    :return:
-    """
-    # get conv arguments
-    kernel_size = conv.kernel_size
-    stride = conv.stride
-    padding = conv.padding
-    dilation = conv.dilation
-
-    # Ref: https://pytorch.org/docs/stable/nn.html
-    h_out = np.floor((h_in + 2 * padding[0] - dilation[0] * (kernel_size[0] - 1) - 1) / stride[0] + 1)
-    w_out = np.floor((w_in + 2 * padding[1] - dilation[1] * (kernel_size[1] - 1) - 1) / stride[1] + 1)
-
-    if pool:
-        h_out /= pool
-        w_out /= pool
-    return int(h_out), int(w_out)
