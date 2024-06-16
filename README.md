483f77a15893352804c5dbb4a1afafa0d2a057a4
Victor N <nwaeze_victor@yahoo.com>
2024-06-16 05:50:20 -0400

    setup can now install torch with gpu support

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 Program that uses deep learning to detect and recognize texts.
 The training data is optimized for subtitle text images.
 
-## Setup Instructions
+## Training Setup Instructions
 
 ### Install Packages:
 
@@ -28,19 +28,21 @@ pip install build
 python -m build
 ```
 
+## Usage
+
 ### Install using pip:
 
 ```
 pip install git+https://github.com/voun-github/Subtitle_OCR.git
 ```
 
-## Usage
+[OCR Models](https://1drv.ms/f/s!AjcgvUnda0Imi_UAUiJ__YWl6D8lZA) - Place downloaded models in `saved models` folder
 
 ``` python
 from sub_ocr import SubtitleOCR
 
-reader = SubtitleOCR() # this needs to run only once to load the models into memory
-result = reader.ocr('image_1.jpg')
+reader = SubtitleOCR()  # this needs to run only once to load the models into memory
+result = reader.ocr("image_1.jpg")
 ```
 
 The output will be in a list format, each item represents a bounding box, the text detected and confident level,
@@ -50,5 +52,3 @@ respectively.
 [{'bbox': ((636, 69), (1284, 72), (1284, 156), (636, 138)), 'text': "Test image text", 'score': 0.8736287951469421},
 {'bbox': ((552, 848), (1364, 864), (1366, 946), (552, 921)), 'text': 'another image text', 'score': 0.8997976183891296}]
 ```
-
-[OCR Models](https://1drv.ms/f/s!AjcgvUnda0Imi_UAUiJ__YWl6D8lZA?e=FkxGab) - Place downloaded models in save models folder 
--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
 from setuptools import setup
 
 setup(
-    name='subtitle_ocr',
-    version='1.0',
+    name="subtitle_ocr",
+    version="1.0",
     packages=[
-        'sub_ocr', 'sub_ocr.models', 'sub_ocr.models.detection', 'sub_ocr.models.detection.db',
-        'sub_ocr.models.detection.db.backbones', 'sub_ocr.models.recognition', 'sub_ocr.models.recognition.alphabets',
-        'sub_ocr.utilities'
+        "sub_ocr", "sub_ocr.models", "sub_ocr.models.detection", "sub_ocr.models.detection.db",
+        "sub_ocr.models.detection.db.backbones", "sub_ocr.models.recognition", "sub_ocr.models.recognition.alphabets",
+        "sub_ocr.utilities"
     ],
     include_package_data=True,
     package_data={"sub_ocr.models.recognition": ["**/*.txt"]},
-    install_requires=['torchvision', 'opencv-python', 'shapely', 'pyclipper'],
-    url='https://github.com/voun-github/Subtitle_OCR',
-    license='',
-    author='Victor N',
-    author_email='nwaeze_victor@yahoo.com',
-    description='Using deep learning with PyTorch for a specialized subtitle text detection and recognition.'
+    install_requires=[
+        "torch@https://download.pytorch.org/whl/cu121/"
+        "torch-2.3.1%2Bcu121-cp312-cp312-win_amd64.whl ;platform_system=='Windows'",
+        "torchvision@https://download.pytorch.org/whl/cu121/"
+        "torchvision-0.18.1%2Bcu121-cp312-cp312-win_amd64.whl ;platform_system=='Windows'",
+        "torchvision;platform_system!='Windows'", "opencv-python", "shapely", "pyclipper",
+    ],
+    url="https://github.com/voun-github/Subtitle_OCR",
+    license="",
+    author="Victor N",
+    author_email="nwaeze_victor@yahoo.com",
+    description="Using deep learning with PyTorch for a specialized subtitle text detection and recognition."
 )
