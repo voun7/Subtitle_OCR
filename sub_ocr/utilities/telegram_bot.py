85d54b11a5b7a9eeeafd192ced540ba7fef13dcd
Victor N <nwaeze_victor@yahoo.com>
2024-06-17 09:14:02 -0400

    telegram creds made optional

--- sub_ocr/utilities/telegram_bot.py
+++ sub_ocr/utilities/telegram_bot.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from pathlib import Path
 
 import requests
 
@@ -7,20 +8,21 @@ logger = logging.getLogger(__name__)
 
 
 class TelegramBot:
-    credential_file = None  # Path file.
+    credential_file: str = None
 
     def __init__(self) -> None:
-        if self.credential_file:
+        if Path(self.credential_file).exists():
             with open(self.credential_file) as file:
                 auth_json = json.load(file)
-            self.bot_token = auth_json["token"]
-            self.chat_id = auth_json["chat_id"]
+            self.bot_token, self.chat_id = auth_json["token"], auth_json["chat_id"]
 
     def send_telegram_message(self, message: str) -> None:
+        if not hasattr(self, "bot_token"):
+            logger.warning(f"Credential not found! Message not sent.")
+            return
         api_url = f'https://api.telegram.org/bot{self.bot_token}/sendMessage'
-        data = {'chat_id': self.chat_id, 'text': message}
         try:
-            response = requests.post(api_url, data=data)
+            response = requests.post(api_url, {'chat_id': self.chat_id, 'text': message})
             response.raise_for_status()
             logger.debug(f"Telegram message sent. Response: {response.text}")
         except Exception as error:
