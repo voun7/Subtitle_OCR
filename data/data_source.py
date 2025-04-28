b587521f95af35eb766131d9928f15b9156d98bb
Victor N <nwaeze_victor@yahoo.com>
2025-04-27 21:25:04 -0400

    dir path changed

--- data/data_source.py
+++ data/data_source.py
@@ -8,7 +8,7 @@ from scipy.io import loadmat
 from sub_ocr.utils import flatten_iter, pairwise_tuples, rect_corners
 from utilities.visualize import visualize_data
 
-DATASET_DIR = Path(r"C:\Users\Victor\Documents\Python Datasets\Subtitle_OCR")
+DATASET_DIR = Path(r"C:\Users\Victor\Documents\Datasets\Subtitle_OCR")
 
 
 # Data Format Annotation
--- sub_ocr/subtitle_ocr.py
+++ sub_ocr/subtitle_ocr.py
@@ -197,7 +197,7 @@ class SubtitleOCR:
 def test_ocr() -> None:
     username = os.getlogin()
     test_image_files = Path(rf"C:\Users\{username}\OneDrive\Public\test images")
-    test_sub_ocr = SubtitleOCR("ch", rf"C:\Users\{username}\OneDrive\Backups\Subtitle OCR Models")
+    test_sub_ocr = SubtitleOCR("ch", rf"C:\Users\{username}\OneDrive\Backups\Models\Subtitle_OCR")
     for test_image in test_image_files.iterdir():
         test_outputs = test_sub_ocr.ocr(str(test_image))
         logger.info(test_image)
